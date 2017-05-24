230ade2be951aa24e46ec786e8364a120009563d
grevillopes <grevillopes@gmail.com>
2017-05-23 19:15:05 -0500

    working program without the UI

--- js/storelocate.js
+++ js/storelocate.js
@@ -2,9 +2,17 @@
  * Created by grevil on 5/23/2017.
  */
 
-$(document).ready(function () {
+var x = document.getElementById("demo");
 
-    "use strict";
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
 
-
-});
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+}
